72860f606c618c3c80ce772bf84c07bae60963f7
Brandon Foos <brandon@brandonfoos.com>
2025-10-28 21:46:27 -0700

    lots of config changes

--- request_cert.sh
+++ request_cert.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-CERT_PATH='./lets-encrypt'
-DOMAIN='guac.foos.net'
-EMAIL='webmaster@foos.net'
+function single_cert() {
+  CERT_PATH='./lets-encrypt'
+  DOMAIN='guac.foos.net'
+  EMAIL='webmaster@foos.net'
+  mkdir -p $CERT_PATH
 
-mkdir -p $CERT_PATH
+  docker run --rm -p 80:80 -p 443:443 \
+      -v ${CERT_PATH}:/etc/letsencrypt \
+      certbot/certbot certonly -d $DOMAIN \
+      --standalone -m $EMAIL --agree-tos
+}
 
-docker run --rm -p 80:80 -p 443:443 \
-    -v ${CERT_PATH}:/etc/letsencrypt \
-    certbot/certbot certonly -d $DOMAIN \
-    --standalone -m $EMAIL --agree-tos
+function wildcard_cert() {
+  CERT_PATH='./lets-encrypt'
+  DOMAIN='*.foos.net'
+  EMAIL='webmaster@foos.net'
+  mkdir -p $CERT_PATH
+
+  docker run --rm -p 80:80 -p 443:443 \
+      -v ${CERT_PATH}:/etc/letsencrypt \
+      -it certbot/certbot certonly \
+      --preferred-challenges dns \
+      -d 'foos.net'
+      -d $DOMAIN \
+      -m $EMAIL
+      --manual
+}
+wildcard_cert
